b7ab66fd9e7a9049cd6bb9314f33c36c02e8f459
Lorenz <lor.els@gmx.de>
2023-09-20 13:38:22 +0200

    - Fixed everything

--- src/main/java/de/subscripted/backend/ButtonInteraction.java
+++ src/main/java/de/subscripted/backend/ButtonInteraction.java
@@ -474,8 +474,7 @@ public class ButtonInteraction extends ListenerAdapter {
 
                 VoiceChannel voiceChannel1 = guild.getVoiceChannelById("1078320408199168131");
                 Member interactionMember = event.getMember();
-                if (interactionMember.getVoiceState().getChannel().asVoiceChannel() != voiceChannel1 || interactionMember.getVoiceState().getChannel().asVoiceChannel() == null) {
-                    EmbedBuilder embedBuilder1 = new EmbedBuilder()
+                if (interactionMember.getVoiceState().getChannel() == null || !interactionMember.getVoiceState().getChannel().equals(voiceChannel1)) {                    EmbedBuilder embedBuilder1 = new EmbedBuilder()
                             .setTitle("Varilx Support")
                             .setColor(Color.YELLOW)
                             .setDescription("Du musst dich in " + voiceChannel1.getAsMention() + " befinden, um den Support dieses Users als abgeschlossen zu aktzeptieren!")
